fe210a9e2dcf58c2d1615b0b2da3555fd3901f3a
Lex Stapleton <lex.stapleton1423@gmail.com>
2026-07-04 15:36:54 -0400

    city building test (not junctions yet)

--- Main/cities/city_layout.gd
+++ Main/cities/city_layout.gd
@@ -14,31 +14,37 @@ extends Resource
 @export var building_types : Array[int]
 
 
-func get_street(x : int, y : int, horizontal : bool) -> bool:
-    var arr = horizontal_streets if horizontal else vertical_streets
+func get_horizontal_street(x : int, y : int) -> bool:
+	if (x < 0) or (x >= size.x) or (y < 0) or (y >= size.y + 1):
+		return false
+	
+	return horizontal_streets[x + y * size.x]
+	
+	
+func get_vertical_street(x : int, y : int) -> bool:
+	if (x < 0) or (x >= size.x + 1) or (y < 0) or (y >= size.y):
+		return false
+	
+	return vertical_streets[y + x * size.y]
 
-    if (x < 0) or (x >= size.x + 1) or (y < 0) or (y >= size.y + 1):
-        return false
-    
-    return arr[x + y * size.x]
 
 
 func get_junction(x : int, y : int) -> bool:
-    if (x < 0) or (x >= size.x + 1) or (y < 0) or (y >= size.y + 1):
-        return false
+	if (x < 0) or (x >= size.x + 1) or (y < 0) or (y >= size.y + 1):
+		return false
 
-    return junctions[x + y * size.x]
+	return junctions[x + y * (size.x  +1)]
 
 
 func get_building_at(x : int, y : int) -> int:
-    if (x < 0) or (x >= size.x) or (y < 0) or (y >= size.y):
-        return false
+	if (x < 0) or (x >= size.x) or (y < 0) or (y >= size.y):
+		return false
 
-    return buildings[x + y * size.x]
+	return buildings[x + y * size.x]
 
 
 func get_building_type(id : int) -> int:
-    if (id < 0) or (id >= building_types.size()):
-        return false
+	if (id < 0) or (id >= building_types.size()):
+		return false
 
-    return building_types[id]
+	return building_types[id]
