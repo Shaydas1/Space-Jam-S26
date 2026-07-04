58c3a883d3de32c7e90b7ded39e682e4008e8e0a
Lex Stapleton <lex.stapleton1423@gmail.com>
2026-07-04 18:45:32 -0400

    stuf

--- Main/minivan/minivan.gd
+++ Main/minivan/minivan.gd
@@ -7,7 +7,7 @@ extends CharacterBody3D
 
 @export var sprite : Sprite3D
 
-@export var forward_direction : Vector3
+@export var true_forward_direction : Vector3
 @export var right_direction : Vector3
 
 @export var max_forward_speed : float
@@ -22,12 +22,15 @@ extends CharacterBody3D
 @export var max_swerve_speed : float
 @export var swerve_decay : float 
 
+@export var turn_rate : float
 
 var accel_falloff_rate : float
 
 @onready var forward_speed : float = base_speed
 @onready var swerve_speed : float = 0
 
+var angle : float = 0.0
+@export var angle_correction_decay : float = 5.0
 
 func update_texture():
 	if abs(swerve_speed) < max_swerve_speed/2:
@@ -55,12 +58,23 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 
+	var turn_input = Input.get_axis("turn_left", "turn_right")
+	
+	rotate_y(-angle)
+	
+	if turn_input != 0:
+		angle += deg_to_rad(-turn_rate * delta * turn_input)
+		
+	elif abs(angle) > deg_to_rad(0.5):
+		angle = lerp(angle, 0.0, angle_correction_decay * delta)
+	else:
+		angle = 0
+	rotate_y(angle)
+
 	var swerve_input = Input.get_axis("left", "right")
 
 	if swerve_input != 0:
 		swerve_speed = max_swerve_speed * swerve_input
-
-
 
 	var inp = Input.get_axis("break", "forward")
 	
@@ -76,7 +90,7 @@ func _physics_process(delta: float) -> void:
 	elif forward_speed < base_speed:
 		forward_speed = lerp(forward_speed, base_speed, speed_up_rate * delta)
 
-	velocity = forward_speed * forward_direction + swerve_speed * right_direction
+	velocity = forward_speed * -transform.basis.z + swerve_speed * transform.basis.x
 
 	move_and_slide()
 	
