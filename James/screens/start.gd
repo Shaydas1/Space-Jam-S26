02a15b8d657deb85bdac4759d67c222a1d9c9ac3
Shaydas1 <43556419+Shaydas1@users.noreply.github.com>
2026-07-04 15:46:49 -0400

    functional start screen to game

--- James/screens/start.gd
+++ James/screens/start.gd
@@ -4,10 +4,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#SceneManager.swap_screen(SceneManager.Screen.Game)
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_start_button_pressed() -> void:
+	SceneManager.swap_screen(SceneManager.Screen.Game)
