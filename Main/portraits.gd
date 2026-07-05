9b655b0774c9996689f13ca8176b15d90623b3d1
Shaydas1 <43556419+Shaydas1@users.noreply.github.com>
2026-07-05 12:22:22 -0400

    Fixed portraits position and scale

--- Main/portraits.gd
+++ Main/portraits.gd
@@ -8,9 +8,9 @@ var current_portrait
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	switch_portrait("Cat")
-	for allowed_portrait in allowed_portraits:
-		get_node(allowed_portrait).scale.x *= 3
-		get_node(allowed_portrait).scale.y *= 3
+	#for allowed_portrait in allowed_portraits:
+	#	get_node(allowed_portrait).scale.x *= 3
+	#	get_node(allowed_portrait).scale.y *= 3
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
